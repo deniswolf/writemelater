63d535669cb078c9d12327eaabdf70af686f5d37
DW <dear.mr.dw@gmail.com>
2012-10-11 03:05:01 +0200

    Ideas Controller writ in code

--- app/controllers/ideas_controller.js
+++ app/controllers/ideas_controller.js
@@ -1,37 +1,114 @@
 var locomotive = require('locomotive'),
-    Controller = locomotive.Controller;
+    Controller = locomotive.Controller,
+    Model = require('../models/idea_model');
 
 var action = new Controller();
 
-// renderers
+// Renderers
 
 action.index = function() {
-  this.render();
+  var self = this;
+  self.user = self.req.user;
+
+  Model.find(null,null,{sort:'updated'}, function(err, data){
+    if (err) console.log('Idea: failed to index with req:',this.req);
+    self.ideas = data;
+    self.render();
+  });
 };
 
 action.show = function() {
-  this.render();
+  var self = this,
+    id = self.param('id');
+    self.user = self.req.user;
+
+  if (id){
+    Model.findById(
+      id,
+      function(err, data){
+    if (err) console.log('Idea: failed to show with req:',this.req);
+        self.idea = data;
+        self.render();
+      }
+    );
+  } else {
+    self.render();
+  }
 };
 
 action.edit = function() {
-  this.render();
+  var self = this,
+    id = self.param('id');
+
+  if (id){
+    Model.findById(
+      id,
+      function(err, data){
+        if (err) console.log('Idea: failed to edit with req:',this.req);
+        self.idea = data;
+        self.render();
+      }
+    );
+  } else {
+    self.redirect('/');
+  }
 };
 
-// doers
+// Doers
 
 action.create = function() {
-  return;
+  var self = this,
+    idea = self.param('idea');
+
+  if (idea){
+    Model.create(
+      idea,
+      function(err){
+        if (err) console.log('Idea: failed to create with req:',this.req);
+        self.redirect('/');
+      }
+    );
+  } else {
+    self.redirect('/');
+  }
 };
 
 action.update = function() {
-  return;
+  var self = this,
+    id = self.param('id'),
+    idea = self.param('idea');
+
+  if (id){
+    Model.findByIdAndUpdate(
+      id,
+      idea,
+      function(err){
+        if (err) console.log('Idea: failed to update with req:',this.req);
+        self.redirect('/');
+      }
+    );
+  } else {
+    self.redirect('/');
+  }
 };
 
 action.destroy = function() {
-  return;
+  var self = this,
+    id = self.param('id');
+    self.user = self.req.user;
+
+  if (id){
+    Model.findByIdAndRemove(
+      id,
+      function(err){
+        if (err) console.log('Idea: failed to destroy with req:',this.req);
+        self.redirect('/');
+      }
+    );
+  } else {
+    self.redirect('/');
+  }
 };
-
-
 
 
 module.exports = action;
