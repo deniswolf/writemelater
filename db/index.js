6770fded0d873ab3d0a3a9d3fbb72b4ad2b63465
DW <dear.mr.dw@gmail.com>
2012-10-11 02:36:48 +0200

    database env added

---
+++ db/index.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose'),
+  config = require('../config/database.json'),
+  connection = mongoose.createConnection(config.host, config.database);
+
+
+module.exports = {
+  connection: connection,
+  type: 'mongoose',
+  mongoose: mongoose
+};
