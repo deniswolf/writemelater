d13eb5a15abf6abb08a5a043deefdcbd38ca0569
DW <dear.mr.dw@gmail.com>
2012-10-12 03:15:50 +0200

    addIdea ajax action

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,5 +1,17 @@
 (function($){
 $(document).ready(function(){
 
+	function addNewIdeaHere () {
+		var el = $(this),
+				self = this;
+
+			$.get('/ideas/new').success(function addToDom(data){
+				el.append(data);
+			});
+	}
+
+	$('div#ideas').click(addNewIdeaHere);
+
+
 });
 })(jQuery);
