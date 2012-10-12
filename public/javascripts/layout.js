f0d1b55a73999160df15d218f7df14ebc508b05e
DW <dear.mr.dw@gmail.com>
2012-10-12 04:09:09 +0200

    Client: Idea save, create, update and re-render if needed

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,16 +1,49 @@
 (function($){
+	var c = function(){
+		console.log(arguments);
+	};
 $(document).ready(function(){
 
-	function addNewIdeaHere () {
-		var el = $(this),
+	function addNewIdea () {
+		var el = $('div#ideas'),
 				self = this;
 
-			$.get('/ideas/new').success(function addToDom(data){
+			$.get('/ideas/new')
+				.success(function addToDom(data){
 				el.append(data);
 			});
 	}
 
-	$('div#ideas').click(addNewIdeaHere);
+	function commitIdea () {
+		var event = arguments[0],
+				form = $(this),
+				self = this,
+				update = (form.find('input[name=_method]').val() === 'put'),
+				data = form.serialize(),
+				url = form.attr('action');
+
+		event.preventDefault();
+
+		$.post(url, data)
+		// if no id - re-render this element as 'edit'
+			.success(function checkIfNew (idea) {
+				if (! update && idea._id) {
+					$.get('/ideas/'+idea._id+'/edit')
+						.success(function(data){
+							c('render stuff from post resp:',data, self);
+							form.html(data);
+						});
+				}
+			})
+			.error(function onError () {
+				window.alert('f-cup in post update!');
+			});
+
+	}
+
+	$('p#new').click(addNewIdea);
+
+	$('#ideas').on('submit','form',commitIdea);
 
 
 });
