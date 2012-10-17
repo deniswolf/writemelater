10eed0db9fcfdf02e976bb461c62d1b5af0be275
DW <dear.mr.dw@gmail.com>
2012-10-18 00:52:51 +0200

    init Drafts

--- app/controllers/drafts_controller.js
+++ app/controllers/drafts_controller.js
@@ -1,34 +1,118 @@
 var locomotive = require('locomotive'),
-    Controller = locomotive.Controller;
+    Controller = locomotive.Controller,
+    Model = require('../models/idea_model');
 
 var action = new Controller();
 
 // renderers
 
 action.index = function() {
-  this.render();
+  var self = this,
+      idea_id = self.param('idea_id');
+      self.idea_id = idea_id;
+
+  Model.findById(
+    idea_id,
+    function(err, idea){
+      if (err) return;
+
+      self.comments = idea.drafts.sort(
+        function(a, b){
+          return b.updated.getTime() - a.updated.getTime();
+        });
+      self.render();
+    }
+  );
 };
 
 action.show = function() {
-  this.render();
+  var self = this,
+    id = self.param(id),
+    idea_id = self.param('idea_id');
+    self.idea_id = idea_id;
+
+  Model.findById(
+    idea_id,
+    function(err, idea){
+      if (err) return;
+
+      self.drafts = idea.drafts.id(id);
+      self.render();
+    }
+  );
 };
 
 action.edit = function() {
-  this.render();
+  var self = this,
+    id = self.param(id),
+    idea_id = self.param('idea_id');
+    self.idea_id = idea_id;
+
+  Model.findById(
+    idea_id,
+    function(err, idea){
+      if (err) return;
+
+      self.draft = idea.drafts.id(id);
+      self.render();
+    }
+  );
 };
 
 // doers
 
 action.create = function() {
-  return;
+  var self = this,
+    idea_id = self.param('idea_id'),
+    draft = self.param('draft');
+
+  Model.findById(
+    idea_id,
+    function(err, idea){
+      if (err) return;
+        idea.drafts.push(draft);
+        idea.save(function(){
+          self.render();
+        });
+    }
+  );
 };
 
 action.update = function() {
-  return;
+  var self = this,
+    idea_id = self.param('idea_id'),
+    id = self.param('id'),
+    draft = self.param('draft');
+
+  Model.findById(
+    idea_id,
+    function(err, idea){
+      if (err) return;
+        idea.drafts.id(id).remove();
+        idea.drafts.push(draft);
+        idea.save(function(){
+          self.render();
+        });
+    }
+  );
 };
 
 action.destroy = function() {
-  return;
+  var self = this,
+    idea_id = self.param('idea_id'),
+    id = self.param('id'),
+    draft = self.param('draft');
+
+  Model.findById(
+    idea_id,
+    function(err, idea){
+      if (err) return;
+        idea.drafts.id(id).remove();
+        idea.save(function(){
+          self.render();
+        });
+    }
+  );
 };
 
 
