99d4b872d18da4364e30c13858affc68b40b5eda
DW <dear.mr.dw@gmail.com>
2012-10-23 01:49:32 +0200

    prettify code

--- public/javascripts/writemelater.js
+++ public/javascripts/writemelater.js
@@ -1,16 +1,16 @@
 (function($){
 $('document').ready(function(){
 
-  // data model
-  var Idea = function(text, children) {
-    this.text = ko.observable(text);
-    this.children = ko.observableArray(children || []);
-  };
-
   var ipsum = {
     short:"I thought what I'd do was, I'd pretend I was one of those deaf-mutes.",
     medium:"That way I wouldn't have to have any goddamn stupid useless conversations with anybody. If anybody wanted to tell me something they'd have to write it on a piece of paper and shove it over to me. They'd get bored as hell doing that after a while, and then I'd be through with having conversations for the rest of my life.",
     long:"Here's to the crazy ones\nThe misfits\nThe rebels\nThe troublemakers\nThe round pegs in the square holes\nThe ones who see things differently\nThey're not fond of rules\nAnd they have no respect for the status quo\nYou can quote them, disagree with them, glorify or vilify them\nAbout the only thing you can't do is ignore them\nBecause they change things\nThey push the human race forward\nAnd while some may see them as the crazy ones, we see genius\nBecause the people who are crazy enough to think they can change the world, are the ones who do.",
+  };
+
+  // data model
+  var Idea = function(text, children) {
+    this.text = ko.observable(text);
+    this.children = ko.observableArray(children || []);
   };
 
   // mmvc model
@@ -29,8 +29,6 @@ $('document').ready(function(){
           parentArray.unshift(new Idea(text));
       };
   };
-
-  window.i = ko.applyBindings(new IdeaMMVCModel());
 
   //attach event handlers
   $("#ideas").on("click", ".remove", function() {
