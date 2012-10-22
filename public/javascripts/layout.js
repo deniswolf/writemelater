335a8b8164c5758373370c0371376ce48d9a554f
DW <dear.mr.dw@gmail.com>
2012-10-23 00:47:50 +0200

    textarea autoresize on edit

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,5 +1,27 @@
 (function($){
 $(document).ready(function(){
 
+function resizeElement (e) {
+  var offsetHeight = e.offsetHeight - e.clientHeight;
+
+  e.style.height = 'auto';
+  e.style.height = (e.scrollHeight  + offsetHeight ) + 'px';
+
+  var offsetWidth = e.offsetWidth - e.clientWidth;
+
+  e.style.width = 'auto';
+  e.style.width = (e.scrollWidth  + offsetWidth ) + 'px';
+}
+
+$('#ideas').on('input, keyup', 'textarea', function autoResize () {
+  resizeElement(this);
+});
+
+$.each(
+  $('#ideas textarea'),
+  function(i,domElement){ resizeElement(domElement);}
+);
+
+
 });
 })(jQuery);
