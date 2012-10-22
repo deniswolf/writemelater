e82c8d0e03fc026490a3f9bc971f9095ef7bfa6d
DW <dear.mr.dw@gmail.com>
2012-10-22 23:43:34 +0200

    poke it!

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,19 +1,5 @@
 (function($){
 $(document).ready(function(){
-	var $ideas = $.writemelater.ideas;
-
-	$ideas.enableDeleteIdeaOnBackspace();
-
-	$('p#new').click($ideas.addNewIdea);
-
-	$('#ideas').on('submit','form', $ideas.commitIdea);
-
-	$('#ideas').on('update', function rerenderIdeas () {
-		$(this).html('');
-		$ideas.addAllIdeas($(this));
-	});
-
-	$('#ideas').trigger('update');
 
 });
 })(jQuery);
--- public/javascripts/writemelater.js
+++ public/javascripts/writemelater.js
@@ -1,8 +1,62 @@
 (function($){
+$('document').ready(function(){
+
+  // data model
+  var Idea = function(text, children) {
+    this.text = ko.observable(text);
+    this.children = ko.observableArray(children || []);
+  };
+
+  // mmvc model
+  var IdeaMMVCModel = function() {
+      this.ideas = ko.observableArray([
+          new Idea("Bob", [
+              new Idea("Jan"),
+              new Idea("Don", [
+                  new Idea("Doug")
+              ])
+          ]),
+          new Idea("Ann", [
+              new Idea("Eve"),
+          ])
+      ]);
+
+      this.addChild = function(text, parentArray) {
+          parentArray.push(new Idea(text));
+      };
+  };
+
+  window.i = ko.applyBindings(new IdeaMMVCModel());
+
+  //attach event handlers
+  $("#ideas").on("click", ".remove", function() {
+      //retrieve the context
+      var context = ko.contextFor(this),
+          parentArray = context.$parent.ideas || context.$parent.children;
+
+      //remove the data (context.$data) from the appropriate array on its parent (context.$parent)
+      parentArray.remove(context.$data);
+
+      return false;
+  });
+
+  $("#ideas").on("click", ".add", function() {
+      //retrieve the context
+      var context = ko.contextFor(this),
+          childName = context.$data.text() + " child",
+          parentArray = context.$data.ideas || context.$data.children;
+
+      //add a child to the appropriate parent, calling a method off of the main view model (context.$root)
+      context.$root.addChild(childName, parentArray);
+
+      return false;
+  });
+
+  $("#new").on("click", ".create", function(){
+    var ideas = ko.contextFor($('#ideas')[0]).$root.ideas;
+    ideas.push(new Idea('nya!'));
+  });
 
 
-  //define projects helpers lib
-  $.writemelater = {};
-
-
+});
 })(jQuery);
